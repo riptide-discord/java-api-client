19615dd7020dca5af2376eb3e2358ae6ae8eb079
Zak <shearmanzak@gmail.com>
2021-06-16 12:29:31 +0100

    Clean up gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     java
+    `maven-publish`
 }
 
 group = "pink.zak.client.wavybot"
@@ -13,11 +14,4 @@ dependencies {
     implementation("org.jetbrains:annotations:16.0.2")
     annotationProcessor("org.jetbrains:annotations:16.0.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
 }
